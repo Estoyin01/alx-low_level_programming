4d44cbff5ee9704563a8520758da7cf732dd724d
Oluwatoyin Abiodun <oluwatoyinabiodun997@gmail.com>
2023-09-17 12:14:12 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 /**
- * main-entry point
- *
- * Return:Always 0 (Success)
- */
+main - entry point
+
+Return: Always 0 (Success)
+ /
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual
-			puzzle");
+puts("\"Programming is like building a multilingual
+puzzle");
 
-	return (0);
+return (0);
 }
