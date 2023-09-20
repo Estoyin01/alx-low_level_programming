fbdecda4ab4e8fd8259087144fa8c7291c28a286
Oluwatoyin Abiodun <oluwatoyinabiodun997@gmail.com>
2023-09-20 11:36:09 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+/**
+ * main - Print sizeof
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+
+	printf("Size of a char: %lu byte(s)\n", sizeof(a));
+	printf("Size of an int: %lu byte(s)\n", sizeof(b));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(f));
+
+return (0);
+}
+
